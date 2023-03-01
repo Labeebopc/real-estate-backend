53a82df484eb1915e4ae08865446341e7348f89c
Labeebopc <Labeebopc313@gmail.com>
2023-03-01 11:15:42 +0530

    routes, controllers are added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const env = require("dotenv").config()
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
+const propertyRoutes=require('./routes/PropertyRoutes.js')
 
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/api/v1/add-new-property',propertyRoutes)
 
 
 //Defie PORT and Database
