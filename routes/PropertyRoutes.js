13c5e6ea42fbd02063ca02d3579592db872b5daf
Labeebopc <Labeebopc313@gmail.com>
2023-03-05 20:30:50 +0530

    cors added

--- routes/PropertyRoutes.js
+++ routes/PropertyRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { createProperty } = require('../controllers/propertyController.js')
+const { createProperty, createNewBasicInfo, createNewPropertyDetails, createNewGeneralInfo, createNewLocationInfo } = require('../controllers/propertyController.js')
 
 const router = express.Router()
 
@@ -8,6 +8,11 @@ const router = express.Router()
 //Create a Posts
 //Use verify token as middleware
 router.post("/", createProperty)
+
+router.post("/basicinfo", createNewBasicInfo)
+router.post("/propertydetails", createNewPropertyDetails)
+router.post("/generalinfo", createNewGeneralInfo)
+router.post("/locationinfo", createNewLocationInfo)
 
 
 module.exports = router;
